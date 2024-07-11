d9bbe6ca7671d66ecb917b21e7b2aee338e9b414
infotus <48644796+infotus@users.noreply.github.com>
2024-07-11 17:57:12 +0900

    noe text update

--- src/components/Pages/Noe-Talk/ProjectNoe.jsx
+++ src/components/Pages/Noe-Talk/ProjectNoe.jsx
@@ -4,9 +4,11 @@ import noeimg from '../../../assets/noe-talk.png'
 function ProjectNoe() {
   return (
     <div >
-      <p className="p-10 test-center py-8">
-        "We are excited to announce the launch of our new AI Conversation feature Noe-bot, designed to enhance user interactions and streamline communication. This innovative tool leverages advanced artificial intelligence to provide real-time, intuitive responses, making it easier than ever to get the information you need."
-      </p>
+      <section className='h-[50%] w-full pt-20 pb-10'>
+        <p className="p-10 test-center py-8">
+          "We are excited to announce the launch of our new AI Conversation feature Noe-bot, designed to enhance user interactions and streamline communication. This innovative tool leverages advanced artificial intelligence to provide real-time, intuitive responses, making it easier than ever to get the information you need."
+        </p>
+      </section>
       <div className="relative flex justify-center items-center h-auto w-[60%] shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] left-[20%]">
           <img src={noeimg} alt="/" className="p-10 rounded-xl group-hover:opacity-10"/>
           <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
