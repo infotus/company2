5eeb59314da997c60426878df71b8ee2b24d1985
infotus <48644796+infotus@users.noreply.github.com>
2024-07-31 17:32:42 +0900

    About page updated

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import test from '../../../assets/logo_dark.png'
 
 function About() {
   return (
-    <div className='grid grid-rows-1 gap-16 bg-cyan-950 min-w-[1000px] min-h-[1000px] justify-center items-center text-center text-4xl'>
-      About Us
+    <div className='grid grid-cols-1 bg-cyan-950 place-items-center text-center gap-4 p-2 m-auto pt-32'>
+      
+      <div className='p-2 gap-2'>
+        <h1 className='text-3xl'>Our Policy</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, totam velit molestiae similique porro maiores aliquid eum perferendis blanditiis delectus animi dicta obcaecati accusamus quibusdam assumenda at! Consequatur, vel magnam?</p>
+      </div>
+
+      <div className='p-2 gap-2'>
+        <h1 className='text-3xl'>Our Mission</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab, expedita in nobis quas iure corrupti culpa quisquam, harum voluptate nesciunt quod eos quia aperiam fuga optio sit nam error.</p>
+      </div>
+      
+      <div className='p-2 gap-2 place-items-center'>
+        <h1 className='text-3xl'>Our Story</h1>
+        <div className=''>
+          {/* Co-Founder Info */}
+          <div className='grid grid-cols-1 md:grid-cols-2 p-2 gap-2'>
+            <img className='w-[50%] md:w-[30%]' src={test} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rerum vel nemo ad ipsum fuga voluptates, nisi excepturi eos, quis eius. Id minima sed asperiores, itaque excepturi similique nisi quae?</p>
+          </div>
+          {/* Co-Founder Info */}
+          <div className='grid grid-cols-1 md:grid-cols-2 p-2 gap-2'>
+            <img className='w-[50%] md:w-[30%]' src={test} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rerum vel nemo ad ipsum fuga voluptates, nisi excepturi eos, quis eius. Id minima sed asperiores, itaque excepturi similique nisi quae?</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
