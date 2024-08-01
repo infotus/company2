133a6900ca5712ae1b946b3c9ee245b0c96493d1
infotus <48644796+infotus@users.noreply.github.com>
2024-08-01 17:59:45 +0900

    about page updated

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -3,29 +3,29 @@ import test from '../../../assets/logo_dark.png'
 
 function About() {
   return (
-    <div className='grid grid-cols-1 bg-cyan-950 place-items-center text-center gap-4 p-2 m-auto pt-32'>
+    <div className='bg-cyan-950 grid place-items-center gap-14 p-2 pt-32'>
       
       <div className='p-2 gap-2'>
-        <h1 className='text-3xl'>Our Policy</h1>
+        <h1 className='grid place-content-center text-3xl p-2'>Our Policy</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, totam velit molestiae similique porro maiores aliquid eum perferendis blanditiis delectus animi dicta obcaecati accusamus quibusdam assumenda at! Consequatur, vel magnam?</p>
       </div>
 
       <div className='p-2 gap-2'>
-        <h1 className='text-3xl'>Our Mission</h1>
+        <h1 className='grid place-content-center text-3xl p-2'>Our Mission</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ab, expedita in nobis quas iure corrupti culpa quisquam, harum voluptate nesciunt quod eos quia aperiam fuga optio sit nam error.</p>
       </div>
       
-      <div className='p-2 gap-2 place-items-center'>
-        <h1 className='text-3xl'>Our Story</h1>
-        <div className=''>
+      <div>
+        <h1 className='grid place-content-center text-3xl p-6'>Our Background</h1>
+        <div className='grid grid-cols-1 gap-14 md:grid-cols-2'>
           {/* Co-Founder Info */}
-          <div className='grid grid-cols-1 md:grid-cols-2 p-2 gap-2'>
-            <img className='w-[50%] md:w-[30%]' src={test} alt="" />
+          <div className='grid gap-5 p-2 md:grid-cols-2 place-items-center'>
+            <img className='grid md:grid-cols-2 w-[50%] md:w-[60%]' src={test} alt="" />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rerum vel nemo ad ipsum fuga voluptates, nisi excepturi eos, quis eius. Id minima sed asperiores, itaque excepturi similique nisi quae?</p>
           </div>
           {/* Co-Founder Info */}
-          <div className='grid grid-cols-1 md:grid-cols-2 p-2 gap-2'>
-            <img className='w-[50%] md:w-[30%]' src={test} alt="" />
+          <div className='grid gap-5 p-2 md:grid-cols-2 place-items-center'>
+            <img className='grid md:grid-cols-2 w-[50%] md:w-[60%]' src={test} alt="" />
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores rerum vel nemo ad ipsum fuga voluptates, nisi excepturi eos, quis eius. Id minima sed asperiores, itaque excepturi similique nisi quae?</p>
           </div>
         </div>
