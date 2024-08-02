e971298d5257ed4615934f1383e21542701c537c
infotus <48644796+infotus@users.noreply.github.com>
2024-08-02 12:50:26 +0900

    edit files for publishing on vercel

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/company2/",
+  // Add this in package json file ->  "homepage": "https://github.io/infotus/company2",
   plugins: [react()],
   css: {
     postcss: {
